6fad5b1faeec69ffaf9d337e047fdbc66f5565d3
gzaxar <78252867+gzaxar@users.noreply.github.com>
2021-10-17 22:58:13 +0300

    Add files via upload

---
+++ Files/main.py
@@ -0,0 +1,89 @@
+# Change to the data directory
+import os
+os.chdir('.') # You should change to the corresponding directory in your computer
+os.listdir()
+# Import numpy, pandas for data manipulation
+import numpy as np
+import pandas as pd
+
+# Import matplotlib, seaborn for visualization
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+# Import the data
+weather_data = pd.read_csv('weather_data_test.csv',parse_dates=['datetime'], sep=';', decimal=','
+                     , infer_datetime_format=True)
+
+# Check the shape of the dataset
+print(weather_data.shape)
+
+# Select the datetime and the temperature columns
+temp_df = weather_data[["datetime","T_mu"]]
+temp_df.head(10)
+# Check the shape of the dataset
+temp_df.shape
+# Select the subset data from 2016 to 2019
+mask = (temp_df['datetime'] >= '2015-09-07') & (temp_df['datetime'] <= '2019-05-21')
+temp_df = temp_df.loc[mask]
+
+# Reset the index
+temp_df.set_index("datetime", inplace=True)
+
+# Inspect first 5 rows and last 5 rows of the data
+from IPython.display import display
+display(temp_df.head(5))
+display(temp_df.tail(5))
+temp_df.describe()
+# Output the maximum and minimum temperature date
+print(temp_df.loc[temp_df["T_mu"] == temp_df["T_mu"].max()])
+print(temp_df.loc[temp_df["T_mu"] == temp_df["T_mu"].min()])
+# Plot the daily temperature change
+plt.figure(figsize=(16,10), dpi=100)
+plt.plot(temp_df.index, temp_df.T_mu, color='tab:red')
+plt.gca().set(title="Daily Temperature in Thessaloniki, Greece from 2016 to 2019", xlabel='Date', ylabel="Degree (in Celsius)")
+plt.show()
+
+# Apply the Moving Average function by a subset of size 30 days.
+temp_df_mean = temp_df.T_mu.rolling(window=30).mean()
+temp_df_mean.plot(figsize=(20,15))
+from statsmodels.tsa.seasonal import seasonal_decompose, DecomposeResult
+
+# Additive Decomposition
+# result_add: DecomposeResult = seasonal_decompose(temp_df.T_mu, model='additive', extrapolate_trend='freq', freq=365)
+
+
+# Plot
+plt.rcParams.update({'figure.figsize': (10,10)})
+# result_add.plot().suptitle('Additive Decomposition', fontsize=22)
+plt.show()
+
+# Shift the current temperature to the next day.
+predicted_df = temp_df["T_mu"].to_frame().shift(1).rename(columns = {"T_mu": "T_mu_pred" })
+actual_df = temp_df["T_mu"].to_frame().rename(columns = {"T_mu": "T_mu_actual" })
+
+# Concatenate the actual and predicted temperature
+one_step_df = pd.concat([actual_df,predicted_df],axis=1)
+
+# Select from the second row, because there is no prediction for today due to shifting.
+one_step_df = one_step_df[1:]
+one_step_df.head(10)
+
+# Shift the current temperature to the next day.
+predicted_df = temp_df["T_mu"].to_frame().shift(1).rename(columns = {"T_mu": "T_mu_pred" })
+actual_df = temp_df["T_mu"].to_frame().rename(columns = {"T_mu": "T_mu_actual" })
+
+# Concatenate the actual and predicted temperature
+one_step_df = pd.concat([actual_df,predicted_df],axis=1)
+
+# Select from the second row, because there is no prediction for today due to shifting.
+one_step_df = one_step_df[1:]
+one_step_df.head(10)
+
+from sklearn.metrics import mean_squared_error as MSE
+from math import sqrt
+
+# Calculate the RMSE
+temp_pred_err = MSE(one_step_df.T_mu_actual, one_step_df.T_mu_pred, squared=False)
+print("The RMSE is",temp_pred_err)
